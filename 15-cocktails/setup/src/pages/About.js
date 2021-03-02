012c8c5ba97d89b7772fd99a67064dd01dbbecf5
petter0619 <petter0619@gmail.com>
2021-03-02 20:07:41 +0100

    Project 15 - Cocktail DB

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+      Feed in a woof, a farmers market. Kidney beans ostrich trucks. Grapes at yams mushrooms organic berries gobble. Grapes at yams mushrooms organic berries gobble. Cat at pineapples pigeons. Combine Harvester swather, baler as haybine parsley, melon in hay rake. Bull. At rose garden cucumbers mice sunflower wheat in pig. Prairie dogs raccoons robins rats. In the straw rain barrels. Feed in a woof, a farmers market. Bulls at rose garden cucumbers mice sunflower wheat in pig. In the straw rain barrels. Oranges cucumbers rhubarb gourds watermelon. Peacocks baa ostriches owls. Gourds utters at welding equipment a oink oink haybine. bull bowels cat chicken cow, calf donkey duck. Gourds utters at welding equipment .
+      </p>
+    </section>
   )
 }
 
