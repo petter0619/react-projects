f14563c68d524bb8f7bef52341aaea36f4142f93
petter0619 <petter0619@gmail.com>
2021-03-02 16:34:39 +0100

    Project 14 - useReducer Shopping Cart

--- 14-cart/setup/src/CartContainer.js
+++ 14-cart/setup/src/CartContainer.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import CartItem from './CartItem'
-import { useGlobalContext } from './context'
+import React from 'react';
+import CartItem from './CartItem';
+import { useGlobalContext } from './context';
 
 const CartContainer = () => {
-  const { cart } = useGlobalContext()
+  const { cart, total, clearCart } = useGlobalContext()
   if (cart.length === 0) {
     return (
       <section className='cart'>
@@ -32,18 +32,15 @@ const CartContainer = () => {
         <hr />
         <div className='cart-total'>
           <h4>
-            total <span>$0.00</span>
+            total <span>${total}</span>
           </h4>
         </div>
-        <button
-          className='btn clear-btn'
-          onClick={() => console.log('clear cart')}
-        >
+        <button className='btn clear-btn' onClick={clearCart}>
           clear cart
         </button>
       </footer>
     </section>
   )
-}
+};
 
-export default CartContainer
+export default CartContainer;
